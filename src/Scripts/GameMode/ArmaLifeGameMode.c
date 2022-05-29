cd03fe85e3797b191876f64300fabd10511d8068
maltesermailo <jannik@andreasmueller.de>
2022-05-29 19:46:56 +0200

    Init

---
+++ src/Scripts/GameMode/ArmaLifeGameMode.c
@@ -0,0 +1,11 @@
+[EntityEditorProps(category: "GameScripted/ArmaLifeGameMode", description: "ArmaLife game mode script.", color: "0 0 255 255")]
+class SCR_GameModeCampaignMPClass: SCR_BaseGameModeClass
+{
+};
+
+class SCR_ArmaLifeGameMode : SCR_BaseGameMode
+{
+	override void EOnInit(IEntity owner) {
+		super.EOnInit(owner);
+	}
+}
