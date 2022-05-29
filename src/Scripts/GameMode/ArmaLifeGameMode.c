8a85e2e1f73c06ea93b8674152b5da67f2a721c0
Nightloewe <nightloewe@gmail.com>
2022-05-30 01:04:33 +0200

    Added Respawn Handler Component class

--- src/Scripts/GameMode/ArmaLifeGameMode.c
+++ src/Scripts/GameMode/ArmaLifeGameMode.c
@@ -1,11 +1,14 @@
 [EntityEditorProps(category: "GameScripted/ArmaLifeGameMode", description: "ArmaLife game mode script.", color: "0 0 255 255")]
-class SCR_GameModeCampaignMPClass: SCR_BaseGameModeClass
+class ARL_ArmaLifeGameModeClass: SCR_BaseGameModeClass 
 {
 };
 
-class SCR_ArmaLifeGameMode : SCR_BaseGameMode
+class ARL_ArmaLifeGameMode : SCR_BaseGameMode
 {
 	override void EOnInit(IEntity owner) {
 		super.EOnInit(owner);
+		
+		//Override the respawn handler
+		this.m_pRespawnHandlerComponent = SCR_RespawnHandlerComponent.Cast(owner.FindComponent(ARL_RespawnHandlerComponent));
 	}
 }
---
+++ src/Scripts/GameMode/Respawn/ARL_RespawnHandlerComponent.c
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------------------------
+class ARL_RespawnHandlerComponentClass: SCR_RespawnHandlerComponentClass
+{
+};
+
+//------------------------------------------------------------------------------------------------
+/*
+	The RespawnHandlerComponent adds the spawn menu for Arma Life to the game. This includes the
+	selection of the role for the user (Faction), as well as the spawn location.
+*/
+class ARL_RespawnHandlerComponent: SCR_RespawnHandlerComponent {
+
+}
